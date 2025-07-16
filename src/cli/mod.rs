097caf499c458533fc494edf8fd5707923d8444f
wHoIsDReAmer <lcw060403@gmail.com>
2025-07-16 11:54:21 +0900

    fix: fixed visibility

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,2 +1,14 @@
-pub mod cli;
 pub mod commands;
+
+use clap::{command, Parser};
+use crate::cli::commands::Commands;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub(crate) struct Cli {
+    #[arg(short, long, default_value = "./")]
+    pub(crate) directory: String,
+
+    #[command(subcommand)]
+    pub(crate) command: Commands
+}
--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,7 @@ mod dsl;
 mod cli;
 
 use clap::Parser;
-
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-struct Cli {
-    #[arg(short, long, default_value = "./")]
-    directory: String,
-
-    #[command(subcommand)]
-    command: Commands
-}
-
-#[derive(Parser)]
-enum Commands {
-    /// Initialize
-    Init {
-        #[arg(short, long)]
-        name: String,
-    },
-    /// Running tests in project
-    Test {
-        #[arg(short, long, default_value_t = false)]
-        verbose: bool,
-    },
-}
+use cli::Cli;
 
 fn main() {
     let cli = Cli::parse();
