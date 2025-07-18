65478450c1e96a04e0c7a36f611fdbc9232a3597
wHoIsDReAmer <lcw060403@gmail.com>
2025-07-19 00:08:35 +0900

    fix(lexer): fixed tokenizing logic and added full test for lexer

--- src/dsl/lexer.rs
+++ src/dsl/lexer.rs
@@ -40,7 +40,10 @@ impl Lexer {
     }
 
     pub fn is_whitespace(&self) -> bool {
-        self.last_char == ' ' || self.last_char == '\t' || self.last_char == '\n'
+        self.last_char == ' '
+            || self.last_char == '\t'
+            || self.last_char == '\n'
+            || self.last_char == '\r'
     }
 
     pub fn is_first_column(&self) -> bool {
@@ -53,6 +56,10 @@ impl Lexer {
 
     pub fn is_underscore(&self) -> bool {
         self.last_char == '_'
+    }
+
+    pub fn is_dash(&self) -> bool {
+        self.last_char == '-'
     }
 
     pub fn is_digit(&self) -> bool {
@@ -96,6 +103,7 @@ impl Lexer {
 
         if self.is_newline() {
             self.row -= 1;
+            // TODO: Column 처리 필요
             self.column = 0;
         } else {
             self.column -= 1;
@@ -131,7 +139,7 @@ impl Lexer {
             let mut identifier = vec![self.last_char];
 
             self.next();
-            while self.is_alphabet() || self.is_underscore() {
+            while self.is_alphabet() || self.is_underscore() || self.is_dash() {
                 identifier.push(self.last_char);
                 self.next();
             }
@@ -218,7 +226,15 @@ impl Lexer {
             return Ok(Token::Literal(String::from_iter(string)));
         }
 
-        Ok(Token::EOF)
+        if self.is_eof() {
+            return Ok(Token::EOF);
+        }
+
+        Err(TokenError::InvalidToken {
+            last_token_string: self.last_char.to_string(),
+            line: self.row,
+            column: self.column,
+        })
     }
 
     // 생성 없이 가져올 수 있는 유틸 함수
@@ -239,7 +255,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_tokenize_1() {
+    fn test_simple_tokenize_1() {
         let tokens = Lexer::string_to_tokens("123 \"test\"\n  ").unwrap();
 
         assert_eq!(tokens[0], Token::Number(123));
@@ -248,12 +264,48 @@ mod tests {
     }
 
     #[test]
-    fn test_tokenize_2() {
+    fn test_simple_tokenize_2() {
         let tokens = Lexer::string_to_tokens("test 42 \"hello\" \n endpoint").unwrap();
 
         assert_eq!(tokens[0], Token::Test);
         assert_eq!(tokens[1], Token::Number(42));
         assert_eq!(tokens[2], Token::Literal("hello".to_string()));
         assert_eq!(tokens[3], Token::Endpoint);
+    }
+
+    #[test]
+    fn test_verbose_tokenize() {
+        let raw_tokens = r#"
+test "Get User Info"
+endpoint "https://api.example.com/users/123"    
+headers
+  Authorization "Bearer your-token-here"
+  Content-Type "application/json"
+expect
+  status 200
+  body contains "\"username\": \"john\""
+"#;
+
+        let tokens = Lexer::string_to_tokens(raw_tokens).unwrap();
+
+        assert_eq!(tokens[0], Token::Test);
+        assert_eq!(tokens[1], Token::Literal("Get User Info".to_string()));
+        assert_eq!(tokens[2], Token::Endpoint);
+        assert_eq!(tokens[3], Token::Literal("https://api.example.com/users/123".to_string()));
+        assert_eq!(tokens[4], Token::Headers);
+        assert_eq!(tokens[5], Token::Indent);
+        assert_eq!(tokens[6], Token::Item("AUTHORIZATION".to_string()));
+        assert_eq!(tokens[7], Token::Literal("Bearer your-token-here".to_string()));
+        assert_eq!(tokens[8], Token::Indent);
+        assert_eq!(tokens[9], Token::Item("CONTENT-TYPE".to_string()));
+        assert_eq!(tokens[10], Token::Literal("application/json".to_string()));
+        assert_eq!(tokens[11], Token::Expect);
+        assert_eq!(tokens[12], Token::Indent);
+        assert_eq!(tokens[13], Token::Status);
+        assert_eq!(tokens[14], Token::Number(200));
+        assert_eq!(tokens[15], Token::Indent);
+        assert_eq!(tokens[16], Token::Body);
+        assert_eq!(tokens[17], Token::Contains);
+        assert_eq!(tokens[18], Token::Literal("\"username\": \"john\"".to_string()));
     }
 }
